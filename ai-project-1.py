182bcee6db0e5eecd4ffcbc8f00e94db09eb0701
Spenser Mason <mason11@mail.usf.edu>
2017-09-25 13:04:00 -0400

    Mostly finished setup and input parsing

--- ai-project-1.py
+++ ai-project-1.py
@@ -2,6 +2,7 @@ import sys
 import re
 
 def print_usage():
+    '''Print how to use the function.'''
     print('Usage:')
     print('python ai-project-1.py connections_file locations_file start_city end_city excluded_cities heuristic output')
     print(
@@ -28,6 +29,7 @@ def print_usage():
     print(
 '''\toutput [1, 2]:\t\tWhich type of output to provide. 1 - Optimal path only. 2 - Step-by-step''')
 
+
 def main(args):
     city_name_regex = re.compile(r'''[A-Z]\d{1,3}[a-z]?''')
 
@@ -39,8 +41,24 @@ def main(args):
         for line in locations_file:
             name_match = city_name_regex.search(line)
             if name_match:
-                cities[name_match.group(0)] = {'x': -1, 'y': -1, 'connections': []}
-    print(cities)
+                cities[name_match.group(0)] = {'name': name_match.group(0), 'x': -1, 'y': -1, 'connections': []}
+    
+    with open(args[0], 'r') as connections_file:
+        source_city_regex = re.compile(r'({0}) (\d+) '.format(city_name_regex.pattern))
+        for line in connections_file:
+            source_city_match = source_city_regex.search(line)
+            if not source_city_match:
+                break
+            source_city = source_city_match.group(1)
+            connections_count_match = source_city_regex.search(line)
+            if not connections_count_match:
+                break
+            connections_count = int(connections_count_match.group(2))
+            city_names = city_name_regex.findall(line)
+            for i in range(1, connections_count + 1):
+                cities[source_city]['connections'].append(city_names[i]) #TODO: instead of storing city names in connections, store references to objects
+
+        print(cities)
 
 if __name__ == '__main__':
     if len(sys.argv) != 8:
